0a005fb295d2eef76be9775b317388cb41ba0e86
Ornie Payer <143427730+Ornipay@users.noreply.github.com>
2025-03-07 15:09:19 -0500

    Revert "Merge be-save-slot-#25 to development"

--- character_customization.py
+++ character_customization.py
@@ -223,7 +223,6 @@ def customization_screen(slot: int):
                 # Back button -> go to save slots
                 if back_button.is_clicked(mouse_pos):
                     print("Back button clicked. Going to save slot menu...")
-                    delete_save(slot)
                     running = False
                     save_slots.Screen_SaveSlot()
                     sys.exit()
